d952916ddea33a2d1f510c79e7cedbd3db5683c3
ashmita41 <97587859+ashmita41@users.noreply.github.com>
2025-02-06 20:10:26 +0530

    Added new features like search button, shimmer UI etc
    Fetched Data from Swiggy API dynamically, added shimmer ui for loading ensuring better UX, added login/logout button, added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,61 @@
 import ResCard from "./ResCard";
 import { useEffect, useState } from "react";
-import resList from "../utils/mockData"
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
-    const [listofRestaurents, setListofRestaurent] = useState(resList);
+    // Local State Varible - Super Powerful Variable
+    const [listofRestaurents, setListofRestaurent] = useState([]);
 
-//     Normal Js Variable
+    const [searchText, setsearchText] = useState("");
+    const [filteredRestaurent, setfilteredRestaurent] = useState([]);
 
-    return (
+useEffect(() => {
+    fetchData();
+    },
+    []);
+
+const fetchData = async () => {
+    const data = await fetch
+    ("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // console.log(json);
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setListofRestaurent(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    setfilteredRestaurent(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+};
+
+// ***Conditional Rendering*** 
+// if(listofRestaurents.length === 0){
+//     return <Shimmer />
+// }
+
+// Conditional Rendering using ternary operator
+    return listofRestaurents.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input className="search-box" type="text" value={searchText} onChange={(e) => {
+                        setsearchText(e.target.value);
+                    }}
+                    />
+                    <button onClick={() => {
+                        // Filter the Restaurent Cards & Update the UI
+                        // Search the text 
+
+                    console.log(searchText);
+                    const filteredRestaurent =  listofRestaurents.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                    setfilteredRestaurent(filteredRestaurent);
+                    // We have our list of filtered restro and we just have to update out UI with the filtered Restaurents
+                    // This process will be super fast because of Reconciliation process of React
+
+                    }}
+                    >Search</button>
+
+                </div>
                 <button className="filter-btn" onClick={() => {
                     //Filter Logic Here
                     const filteredList = listofRestaurents.filter(
@@ -21,7 +66,7 @@ const Body = () => {
                 >Top Rated Restaurents</button>
             </div>
             <div className="restro-container">
-                {listofRestaurents.map((restaurent) => (
+                {filteredRestaurent.map((restaurent) => (
                     <ResCard key={restaurent.info.id} resData={restaurent} />
                    ))}
                     
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import {useState, useSTate} from "react";
 
 const Header = () => {
+    const [btnNameReact, setbtnNameReact] = useState("Login");
+
     return (
         <div className="header">
             <img className="logo" src={LOGO_URL} />
@@ -10,6 +13,14 @@ const Header = () => {
                 <li>About Us</li>
                 <li>Contact Us</li>
                 <li>Cart</li>
+                <button className="login" 
+                onClick={() => {
+                    btnNameReact === "Login" 
+                    ? setbtnNameReact("Logout") 
+                    : setbtnNameReact("Login");
+                    }}>
+                        {btnNameReact}
+                    </button>
             </ul>
             </div>
             
